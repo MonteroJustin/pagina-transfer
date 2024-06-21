c8888d3aee0026f0160eecb408224465fa5069e8
Justin Montero <justinmonteroa23@gmail.com>
2024-06-21 17:45:56 -0600

    cambios de justin

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Pagina para ventas tranfer
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://es.wikipedia.org/wiki/Agaricus#/media/Archivo:Agaricus_campestris.jpg"
           target="_blank"
           rel="noopener noreferrer"
-        >
-          Learn React
+        > 
+          hongo monaguillo
         </a>
       </header>
     </div>
