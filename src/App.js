c1e41b88aac9f371422be163e7edb25b431581e7
Justin Montero <justinmonteroa23@gmail.com>
2024-06-21 18:00:26 -0600

    Otro cambio mas el segundo

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         > 
-          hongo monaguillo
+          Paselo entonces
         </a>
       </header>
     </div>
