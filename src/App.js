36539e94f47fe27be019423f8c4244257cb7ad3c
Justin Montero <justinmonteroa23@gmail.com>
2024-06-21 18:07:34 -0600

    Nuevo cambio en el proyecto

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         > 
-          Paselo entonces
+          Paselo entonces PASAEEEELLOO
         </a>
       </header>
     </div>
