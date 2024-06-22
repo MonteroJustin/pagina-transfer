0c02920a15ffdba51347a296f06b5e12bae0690a
Jose <josecamo21093@gmail.com>
2024-06-21 18:21:18 -0600

    prueba2

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         > 
-          Paselo entonces putoSSSSSSSS
+          Justin Careverga
         </a>
       </header>
     </div>
